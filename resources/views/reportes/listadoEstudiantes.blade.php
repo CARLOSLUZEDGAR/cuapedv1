ec8b7cdcdedcecb4c52e38a89827a343479f96e2
CARLOSLUZEDGAR <carlosyluzalberto@gmail.com>
2022-06-24 08:41:53 -0400

    230622 haciendo pruebas en ele sistema

--- resources/views/reportes/listadoEstudiantes.blade.php
+++ resources/views/reportes/listadoEstudiantes.blade.php
@@ -4,7 +4,7 @@
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>LISTADO DE CURSO</title>
+    <title>LISTADO DE CURSO {{$curso->curso_sigla}} "{{$curso->paralelo}}"</title>
     <!-- CSS only -->
     <style>
         @page{
